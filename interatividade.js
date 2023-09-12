7e6a0f0c7a9071d90d2b17a8dcecde088f6a5d2d
WesleyBanagouro <wesley.banagouro@outlook.com>
2023-09-12 01:06:29 -0300

    newsletter

---
+++ interatividade.js
@@ -0,0 +1,10 @@
+var botao = document.getElementById('newsletter-button');
+
+function clicou() {
+  var nome = document.getElementById('nome-newsletter').value;
+  var email = document.getElementById('email-newsletter').value;
+
+  alert(`Olá ${nome}. Obrigado por visitar meu site. O email não foi cadastrado, isso é uma demonstração apenas.`);
+}
+
+botao.addEventListener('click', clicou);
